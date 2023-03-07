4eaa203416db8062d9d5ff8c964c250b4e47b301
reeruryu <h2ju1004@gmail.com>
2023-03-07 16:10:01 +0900

    Initial commit

---
+++ src/main/java/com/seungah/todayclothes/TodayclothesApplication.java
@@ -0,0 +1,13 @@
+package com.seungah.todayclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayclothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayclothesApplication.class, args);
+	}
+
+}
