4cb56f21aae2e089dce63dfcc08497dea65b0ece
reeruryu <h2ju1004@gmail.com>
2023-03-23 04:02:44 +0900

    feat : 이메일 인증을 통한 회원 가입 구현 (#1)

---
+++ src/main/java/com/seungah/todayclothes/util/AuthKeyRedisUtils.java
@@ -0,0 +1,36 @@
+package com.seungah.todayclothes.util;
+
+import com.seungah.todayclothes.common.exception.CustomException;
+import com.seungah.todayclothes.common.exception.ErrorCode;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AuthKeyRedisUtils {
+
+	private static final int VALID_TIME = 10;
+	private final RedisTemplate<String, String> redisTemplate;
+
+	public void put(String email, String authKey) {
+		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+		valueOperations.set(email, authKey, Duration.ofMinutes(VALID_TIME));
+	}
+
+	public String get(String email) {
+		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+		String value = valueOperations.get(email);
+		if (value == null) {
+			throw new CustomException(ErrorCode.NOT_FOUND_AUTH_KEY);
+		}
+		return value;
+	}
+
+	public void delete(String email) {
+		redisTemplate.delete(email);
+	}
+
+}
