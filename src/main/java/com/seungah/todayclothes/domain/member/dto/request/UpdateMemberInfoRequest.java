b82af9b07338d4eb75cfda2c81950e46be846b2c
reeruryu <h2ju1004@gmail.com>
2023-05-06 21:30:50 +0900

    Feat: 회원 성별, 지역 수정 통합 (#29)
    1. dto를 빼먹고 커밋했음

---
+++ src/main/java/com/seungah/todayclothes/domain/member/dto/request/UpdateMemberInfoRequest.java
@@ -0,0 +1,18 @@
+package com.seungah.todayclothes.domain.member.dto.request;
+
+import com.seungah.todayclothes.global.type.Gender;
+import com.seungah.todayclothes.global.type.valid.EnumTypeValid;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class UpdateMemberInfoRequest {
+
+	@NotNull(message = "지역을 입력해 주세요.")
+	private String region; // TODO
+
+	@EnumTypeValid(enumClass = Gender.class, message = "Invalid Gender")
+	@NotNull(message = "성별(MALE, FEMALE)를 선택하세요.")
+	private String gender;
+
+}
