53e0ee8ad181c13870790cb34e16f06a71a72d6a
dmdwn99 <98579376+dmdwn99@users.noreply.github.com>
2023-05-13 19:00:56 +0900

    Feat: Clothes Crawling 및 Controller 구현 #35
    1. ClothesGroup, ClothesType 엔티티 구현
    2. ClothesGroupType 엔티티를 통해 다대다 연관관계 매핑
    3. ClothesType 중복 저장을 위해 복합 기본 키 설정

--- src/main/java/com/seungah/todayclothes/domain/clothes/controller/ClothesController.java
+++ src/main/java/com/seungah/todayclothes/domain/clothes/controller/ClothesController.java
@@ -5,7 +5,6 @@ import com.seungah.todayclothes.domain.clothes.dto.response.TopResponse;
 import com.seungah.todayclothes.domain.clothes.service.ClothesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +18,11 @@ public class ClothesController {
     private final ClothesService clothesService;
 
     @GetMapping("/top")
-    public ResponseEntity<List<TopResponse>> getTopClothes(@AuthenticationPrincipal Long userId) {
-        return clothesService.getTopClothes(userId);
+    public ResponseEntity<List<TopResponse>> getTopClothes(Integer groupNumber) {
+        return clothesService.getTopClothes(groupNumber);
     }
     @GetMapping("/bottom")
-    public ResponseEntity<List<BottomResponse>> getBottomClothes(@AuthenticationPrincipal Long userId) {
-        return clothesService.getBottomClothes(userId);
+    public ResponseEntity<List<BottomResponse>> getBottomClothes(Integer groupNumber) {
+        return clothesService.getBottomClothes(groupNumber);
     }
 }
--- src/main/java/com/seungah/todayclothes/domain/clothes/service/ClothesService.java
+++ src/main/java/com/seungah/todayclothes/domain/clothes/service/ClothesService.java
@@ -3,27 +3,27 @@ package com.seungah.todayclothes.domain.clothes.service;
 
 import com.seungah.todayclothes.domain.clothes.dto.response.BottomResponse;
 import com.seungah.todayclothes.domain.clothes.dto.response.TopResponse;
-import com.seungah.todayclothes.domain.clothes.entity.Bottom;
-import com.seungah.todayclothes.domain.clothes.entity.ClothesGroup;
-import com.seungah.todayclothes.domain.clothes.entity.Top;
+import com.seungah.todayclothes.domain.clothes.entity.*;
 import com.seungah.todayclothes.domain.clothes.repository.BottomRepository;
 import com.seungah.todayclothes.domain.clothes.repository.ClothesGroupRepository;
+import com.seungah.todayclothes.domain.clothes.repository.ClothesTypeRepository;
 import com.seungah.todayclothes.domain.clothes.repository.TopRepository;
-import com.seungah.todayclothes.global.type.ClothesName;
-import com.seungah.todayclothes.global.type.ScheduleType;
+import com.seungah.todayclothes.global.type.Gender;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +31,7 @@ public class ClothesService {
 
     private final TopRepository topRepository;
     private final BottomRepository bottomRepository;
+    private final ClothesTypeRepository clothesTypeRepository;
     private final ClothesGroupRepository clothesGroupRepository;
 
     @Value("${cloud.aws.s3.bucket}")
@@ -45,87 +46,94 @@ public class ClothesService {
 
     private ChromeDriver driver;
 
-
-    public void crawling() {
-
+    public void crawling(){
+        List<ClothesType> clothesTypes = clothesTypeRepository.findAll();
+        for (ClothesType clothesType : clothesTypes) {
+            if (clothesType.getGender() == Gender.MALE){
+                crawlManClothes(clothesType.getType());
+            } else {
+                crawlWomanClothes(clothesType.getType());
+            }
+        }
     }
 
-    public void crawlManClothes(String keyword) {
+    public void crawlManClothes(String type) {
         driver = setDriver();
         driver.executeScript("window.open('about:blank','_blank');");
         List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(0));
 
-        String pageUrl = manUrl + keyword;
+        String pageUrl = manUrl + type;
         driver.get(pageUrl);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".n-search-contents")));
+    
         List<WebElement> elements = driver.findElements(By.className("img-block"));
-
         for (WebElement element : elements) {
             String itemUrl = element.getAttribute("href");
-
-            driver.executeScript("window.open('about:blank','_blank');");
-            List<String> tabs1 = new ArrayList<String>(driver.getWindowHandles());
-            driver.switchTo().window(tabs1.get(1));
-            driver.get(itemUrl);
-
-            String imageUrl = driver.findElement(By.className("product-img")).findElement(By.tagName("img"))
-                    .getAttribute("src");
-
-            driver.close();
-            driver.switchTo().window(tabs.get(0));
-
-            saveManClothes(keyword, itemUrl, imageUrl);
+            String imageUrl = "https:" + element.findElement(By.tagName("img")).getAttribute("data-original")
+                    .replaceAll("_\\d+\\.", "_500.");
+            saveManClothes(type, itemUrl, imageUrl);
         }
         driver.quit();
     }
-    public void crawlWomanClothes(String keyword) {
+
+    public void crawlWomanClothes(String type) {
         driver = setDriver();
         driver.executeScript("window.open('about:blank','_blank');");
         List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(0));
 
-        String pageUrl = womanUrl + keyword;
+        String pageUrl = womanUrl + type;
         driver.get(pageUrl);
-        List<WebElement> elements = driver.findElements(By.tagName("a"));
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".search_result_prd")));
+
+        List<WebElement> elements = driver.findElements(By.className("Mod_link-prod"));
         for (WebElement element : elements) {
-            String itemUrl = element.getAttribute("href");
-            driver.executeScript("window.open('about:blank','_blank');");
-            List<String> tabs1 = new ArrayList<String>(driver.getWindowHandles());
-            driver.switchTo().window(tabs1.get(1));
-            driver.get(itemUrl);
+            String itemUrl = element.findElement(By.tagName("a")).getAttribute("href");
+            String[] imageUrl = element.findElement(By.className("thumb")).getAttribute("style")
+                    .split("\"");
 
-            String imageUrl = driver.findElement(By.className("pdp__image")).findElement(By.tagName("img"))
-                    .getAttribute("src");
-
-            driver.close();
-            driver.switchTo().window(tabs.get(0));
-            saveWoManClothes(keyword, itemUrl, imageUrl);
+            saveWoManClothes(type, itemUrl, imageUrl[1]);
         }
         driver.quit();
     }
-
-    public void saveManClothes(String keyword, String itemUrl, String imageUrl){
-        List<ClothesGroup> clothesGroups = clothesGroupRepository.findByClothesNames(ClothesName.valueOf(keyword));
-        for (ClothesGroup clothesGroup : clothesGroups) {
-            Integer groupNumber = clothesGroup.getGroupNumber();
-            if (groupNumber>=1 && groupNumber <=40) {
-                topRepository.save(Top.of(itemUrl, imageUrl, clothesGroup));
-            }
-            if (groupNumber>=51 && groupNumber <=90) {
-                bottomRepository.save(Bottom.of(itemUrl, imageUrl, clothesGroup));
+    public void saveManClothes(String type, String itemUrl, String imageUrl){
+        boolean isDuplicate = topRepository.existsByItemUrl(itemUrl) || bottomRepository.existsByItemUrl(itemUrl);
+        if (!isDuplicate) {
+            ClothesType clothesType = clothesTypeRepository.findByGenderAndType(Gender.MALE, type);
+            List<ClothesGroupType> clothesGroupTypes = clothesType.getClothesGroupTypes();
+            for (ClothesGroupType clothesGroupType : clothesGroupTypes) {
+                ClothesGroup clothesGroup = clothesGroupType.getClothesGroup();
+                Integer groupNumber = clothesGroup.getGroupNumber();
+                if (groupNumber >= 1 && groupNumber <= 40) {
+                    topRepository.save(Top.of(itemUrl, imageUrl, clothesGroup));
+                }
+                if (groupNumber >= 51 && groupNumber <= 90) {
+                    bottomRepository.save(Bottom.of(itemUrl, imageUrl, clothesGroup));
+                }
             }
         }
     }
-    public void saveWoManClothes(String keyword, String itemUrl, String imageUrl){
-        List<ClothesGroup> clothesGroups = clothesGroupRepository.findByClothesNames(ClothesName.valueOf(keyword));
-        for (ClothesGroup clothesGroup : clothesGroups) {
-            Integer groupNumber = clothesGroup.getGroupNumber();
-            if (groupNumber>=101 && groupNumber <=140) {
-                topRepository.save(Top.of(itemUrl, imageUrl, clothesGroup));
-            }
-            if (groupNumber>=151 && groupNumber <=190) {
-                bottomRepository.save(Bottom.of(itemUrl, imageUrl, clothesGroup));
+    public void saveWoManClothes(String type, String itemUrl, String imageUrl){
+        boolean isDuplicate = topRepository.existsByItemUrl(itemUrl) || bottomRepository.existsByItemUrl(itemUrl);
+        if (!isDuplicate) {
+            ClothesType clothesType = clothesTypeRepository.findByGenderAndType(Gender.FEMALE, type);
+            List<ClothesGroupType> clothesGroupTypes = clothesType.getClothesGroupTypes();
+            for (ClothesGroupType clothesGroupType : clothesGroupTypes) {
+                ClothesGroup clothesGroup = clothesGroupType.getClothesGroup();
+                Integer groupNumber = clothesGroup.getGroupNumber();
+                if (groupNumber >= 101 && groupNumber <= 140) {
+                    topRepository.save(Top.of(itemUrl, imageUrl, clothesGroup));
+                }
+                if (groupNumber >= 151 && groupNumber <= 190) {
+                    bottomRepository.save(Bottom.of(itemUrl, imageUrl, clothesGroup));
+                }
             }
         }
     }
@@ -140,38 +148,25 @@ public class ClothesService {
         return new ChromeDriver(options);
     }
 
-    public ResponseEntity<List<TopResponse>> getTopClothes(Long userId) {
+    public ResponseEntity<List<TopResponse>> getTopClothes(Integer groupNumber) {
         List<TopResponse> topResponseList = new ArrayList<>();
-        //Todo 유저에 대한 추천 옷을 주기 위한 알고리즘 구현
-        LocalDateTime now = LocalDateTime.now().withHour(12).withMinute(0).withSecond(0).withNano(0);
 
-        if (userId == null){
-            // Todo 모델에 날씨랑 랜덤한 일정 넣어서 그룹 아이디 가져오고, 가져온 그룹 아이디에 해당하는 의류 키워드로 List 만들어서 응답
-            // Todo userId가 null이면 그룹 아이디를 통해 옷을 꺼내오는데 그룹 아이디에 날씨 정보가 있으므로 당일 날씨에 대한 그룹 아이디를 cron을 통해 저장할 필요가 있음
-            List<Top> topList = topRepository.findAll();
-            for (Top top : topList) {
-                topResponseList.add(TopResponse.of(top));
-            }
+        List<Top> topList = topRepository.findByClothesGroup(clothesGroupRepository.findByGroupNumber(groupNumber));
+        for (Top top : topList) {
+            topResponseList.add(TopResponse.of(top));
         }
+        Collections.shuffle(topResponseList);
         return ResponseEntity.ok(topResponseList);
     }
 
-    public ResponseEntity<List<BottomResponse>> getBottomClothes(Long userId) {
+    public ResponseEntity<List<BottomResponse>> getBottomClothes(Integer groupNumber) {
         List<BottomResponse> bottomResponseList = new ArrayList<>();
-        LocalDateTime now = LocalDateTime.now().withHour(12).withMinute(0).withSecond(0).withNano(0);
 
-        if (userId == null){
-            List<Bottom> bottomList = bottomRepository.findAll();
-            for (Bottom bottom : bottomList) {
-                bottomResponseList.add(BottomResponse.of(bottom));
-            }
+        List<Bottom> bottomList = bottomRepository.findByClothesGroup(clothesGroupRepository.findByGroupNumber(groupNumber));
+        for (Bottom bottom : bottomList) {
+            bottomResponseList.add(BottomResponse.of(bottom));
         }
+        Collections.shuffle(bottomResponseList);
         return ResponseEntity.ok(bottomResponseList);
-    }
-
-    public static ScheduleType getRandomScheduleType() {
-        ScheduleType[] values = ScheduleType.values();
-        int length = values.length;
-        return values[new Random().nextInt(length)];
     }
 }
