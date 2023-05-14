a0a9e237f18c30c83203922db226ffca32013aaf
reeruryu <h2ju1004@gmail.com>
2023-05-15 04:01:18 +0900

    Fix: AI API 호출 한글 인코딩 해제 (#40)

--- src/main/java/com/seungah/todayclothes/global/ai/service/AiService.java
+++ src/main/java/com/seungah/todayclothes/global/ai/service/AiService.java
@@ -3,7 +3,6 @@ package com.seungah.todayclothes.global.ai.service;
 import static com.seungah.todayclothes.global.exception.ErrorCode.FAILED_CALL_AI_API;
 import static com.seungah.todayclothes.global.exception.ErrorCode.NOT_FOUND_DAILY_WEATHER;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seungah.todayclothes.domain.region.entity.Region;
@@ -13,14 +12,18 @@ import com.seungah.todayclothes.global.ai.dto.AiClothesDto;
 import com.seungah.todayclothes.global.ai.dto.AiScheduleDto;
 import com.seungah.todayclothes.global.exception.CustomException;
 import com.seungah.todayclothes.global.type.TimeOfDay;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
@@ -34,26 +37,32 @@ public class AiService {
 	private String clothesUrl;
 
 	public AiScheduleDto callAiScheduleApi(String title, String region) {
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(scheduleUrl)
-			.queryParam("title", title)
-			.queryParam("region", region);
-
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getMessageConverters().add(new StringHttpMessageConverter(
-			StandardCharsets.UTF_8));
-		String response = restTemplate.getForObject(builder.toUriString(), String.class);
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode rootNode = null;
+		URI uri;
 		try {
-			rootNode = objectMapper.readTree(response);
-		} catch (JsonProcessingException e) {
+			uri = new URIBuilder(scheduleUrl)
+				.addParameter("title", title)
+				.addParameter("region", region)
+				.build();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(uri);
+
+		try {
+			HttpResponse response = client.execute(request);
+			String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(responseBody);
+			String plan = rootNode.get("plan").asText();
+			String regionName = rootNode.get("region").asText();
+
+			return AiScheduleDto.of(plan, regionName);
+		} catch (Exception e) {
 			throw new CustomException(FAILED_CALL_AI_API);
 		}
-		String plan = rootNode.get("plan").asText();
-		String regionName = rootNode.get("region").asText();
-
-		return AiScheduleDto.of(plan, regionName);
 	}
 
 	public AiClothesDto callAiClothesApi(LocalDate date, TimeOfDay timeOfDay,
@@ -63,29 +72,35 @@ public class AiService {
 			.findByDateAndRegion(date.atTime(0, 0), region)
 			.orElseThrow(() -> new CustomException(NOT_FOUND_DAILY_WEATHER));
 
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(clothesUrl)
-			.queryParam("gender", genderType)
-			.queryParam("humidity", dailyWeather.getHumidity())
-			.queryParam("wind_speed", dailyWeather.getWindSpeed())
-			.queryParam("rain", dailyWeather.getRain())
-			.queryParam("temp", dailyWeather.getAvgTemps().get(timeOfDay))
-			.queryParam("schedule", plan);
-
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getMessageConverters().add(new StringHttpMessageConverter(
-			StandardCharsets.UTF_8));
-        String response = restTemplate.getForObject(builder.toUriString(), String.class);
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode rootNode2 = null;
+		URI uri;
 		try {
-			rootNode2 = objectMapper.readTree(response);
-		} catch (JsonProcessingException e) {
+			uri = new URIBuilder(clothesUrl)
+				.addParameter("gender", genderType)
+				.addParameter("humidity", String.valueOf(dailyWeather.getHumidity()))
+				.addParameter("wind_speed", String.valueOf(dailyWeather.getWindSpeed()))
+				.addParameter("rain", String.valueOf(dailyWeather.getRain()))
+				.addParameter("temp", String.valueOf(dailyWeather.getAvgTemps().get(timeOfDay)))
+				.addParameter("schedule", plan)
+				.build();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(uri);
+
+		try {
+			HttpResponse response = client.execute(request);
+			String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(responseBody);
+			String top = rootNode.get("top").asText();
+			String bottom = rootNode.get("bottom").asText();
+
+			return AiClothesDto.of(top, bottom);
+		} catch (Exception e) {
 			throw new CustomException(FAILED_CALL_AI_API);
 		}
-		String top = rootNode2.get("top").asText();
-		String bottom = rootNode2.get("bottom").asText();
-
-		return AiClothesDto.of(top, bottom);
 	}
 }
