3ccba65ed0b763de1e14f0028f66966484898451
reeruryu <h2ju1004@gmail.com>
2023-06-05 16:47:23 +0900

    Refactor: Schedule 쿼리 성능 개선 (#67)
    - Member 프로필 조회 시,  fetch join을 사용하여 Region을 함께 로딩
    - deleteAllByScheduleDetailId DELETE 직접 쿼리 작성

--- src/main/java/com/seungah/todayclothes/domain/member/repository/MemberRepository.java
+++ src/main/java/com/seungah/todayclothes/domain/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import com.seungah.todayclothes.domain.member.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
